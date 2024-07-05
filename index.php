9556187cccd365b03b87b1820bb1acb89442f5bf
AppeTiZer123 <pecthwiengnon@gmail.com>
2024-07-05 17:11:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
     echo "<h1>my repository</h1>"
     echo "<h1>my name is pecth</h1>"
+    echo "<h1>mine mine</h1>"
+    echo "<h1>my repository</h1>"
+    echo "<h1>my name is pecth</h1>"
     echo "<h1>mine mine</h1>" 
     ?>
